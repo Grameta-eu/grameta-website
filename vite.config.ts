cf1a28aa26a261e80f9cc2e4463a7f027680ca78
Grameta-eu <cnctekinimas@gmail.com>
2025-09-16 18:25:14 +0000

    Sitemap fixes v2

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,38 @@ export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
+    // Configure MIME types for XML files
+    middlewareMode: false,
+    fs: {
+      strict: false
+    }
   },
   plugins: [
     react(),
+    // Plugin to handle XML files with correct MIME type
+    {
+      name: 'xml-mime-type',
+      configureServer(server) {
+        server.middlewares.use('/sitemap.xml', (req, res, next) => {
+          res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+          next();
+        });
+        server.middlewares.use('/robots.txt', (req, res, next) => {
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+          next();
+        });
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use('/sitemap.xml', (req, res, next) => {
+          res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+          next();
+        });
+        server.middlewares.use('/robots.txt', (req, res, next) => {
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+          next();
+        });
+      }
+    }
   ],
   resolve: {
     alias: {
